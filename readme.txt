f802b398ff2e3f1ccced8b17a7415941288c40e3
Kevin Brent <kevin@kevinbrent.com>
2022-05-14 11:03:27 -0700

    Test with current version of Woo and WP

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: kevin-brent
 Tags: brand, woocommerce, sync products, sync data, api feed, json feed, data transfer, csv import, data sync
 Requires at least: 5.0
-Tested up to: 5.9.0
+Tested up to: 5.9.3
 Requires PHP: 5.6
 Stable tag: /trunk
 License: GPLv2
--- wp-data-sync-woocommerce.php
+++ wp-data-sync-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Data Sync for WooCommerce
  * Plugin URI:  https://wpdatasync.com/products/
  * Description: Extend WP Data Sync to include additional WooCommecre functionality
- * Version:     2.1.5
+ * Version:     2.1.6
  * Author:      WP Data Sync
  * Author URI:  https://wpdatasync.com
  * License:     GPL2
@@ -11,8 +11,8 @@
  * Text Domain: wp-data-sync-woocommerce
  * Domain Path: /languages
  *
- * WC requires at least: 3.5
- * WC tested up to: 6.1.1
+ * WC requires at least: 4.0
+ * WC tested up to: 6.5.1
  *
  * Package:     WP_DataSync
  */
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $defines = [
-	'WPDS_FOR_WOO_VERSION' => '2.1.5',
+	'WPDS_FOR_WOO_VERSION' => '2.1.6',
 	'WPDS_FOR_WOO_CAP'     => 'manage_options'
 ];
 
